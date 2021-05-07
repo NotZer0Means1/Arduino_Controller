5c01ede1252e11ac1a7e766a1aa6c528bf899009
vovusprog <vovashevalev@gmail.com>
2021-05-07 16:10:43 +0300

    Активность bluetooth выводит список подключенных заранее устройств

--- app/src/main/java/com.example.project/BluetoothActivity.java
+++ app/src/main/java/com.example.project/BluetoothActivity.java
@@ -1,4 +1,43 @@
 package com.example.project;
 
-public class BluetoothActivity {
+import android.app.Activity;
+import android.bluetooth.*;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.arduino_controller_v2.R;
+
+import java.util.Set;
+
+public class BluetoothActivity extends Activity {
+    private static final int REQUEST_ENABLE_BT = 1;
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+
+        if (bluetoothAdapter.isEnabled()) {
+            // bluetooth включен
+            Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+            if(pairedDevices.size() > 0){
+                for(BluetoothDevice device: pairedDevices){
+                    arrayAdapter.add(device.getName());
+                }
+                listView.setAdapter(arrayAdapter);
+            }
+        }
+        else
+        {
+            // Предложение включить bluetooth
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
 }
