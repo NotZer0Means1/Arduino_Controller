f0314e7cd2315cce5e9db83f77b475ab40eaa11e
vovusprog <vovashevalev@gmail.com>
2021-05-09 17:24:23 +0300

    Выделение выбранного элемента из ListView( не готово )

--- app/src/main/java/com.example.project/BluetoothActivity.java
+++ app/src/main/java/com.example.project/BluetoothActivity.java
@@ -1,18 +1,23 @@
 package com.example.project;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.*;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.BlendMode;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.arduino_controller_v2.R;
 
@@ -26,9 +31,15 @@ public class BluetoothActivity extends Activity {
     Button bt;
     ListView listView;
     BluetoothAdapter bluetoothAdapter;
+
     private ArrayList<String> mDevices = new ArrayList<>();
+
     private ArrayAdapter<String> arrayAdapter;
+
     private Set<BluetoothDevice> devices;
+
+    private static int lastClickId = -1;
+
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,14 +62,13 @@ public class BluetoothActivity extends Activity {
         }
         listView.setAdapter(arrayAdapter);
 
-        if ( !bluetoothAdapter.isDiscovering() ){
-            bluetoothAdapter.startDiscovery();
-        }
+
 
         if ( bluetoothAdapter.isDiscovering() ) {
             bluetoothAdapter.cancelDiscovery();
             bluetoothAdapter.startDiscovery();
         }
+
 
         IntentFilter filter = new IntentFilter();
 
@@ -68,7 +78,9 @@ public class BluetoothActivity extends Activity {
 
         registerReceiver(mReceiver, filter);
 
-
+        if ( !bluetoothAdapter.isDiscovering() ){
+            bluetoothAdapter.startDiscovery();
+        }
     }
 
      public void onDestroy () {
@@ -76,22 +88,44 @@ public class BluetoothActivity extends Activity {
             super.onDestroy();
     }
 
+
+    private AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            if( lastClickId != -1 && lastClickId != position ){
+                parent.getChildAt(lastClickId).setBackgroundResource(R.color.white);
+                view.setBackgroundResource(R.color.white);
+            }
+            if ( lastClickId == -1 ){
+                view.setBackgroundResource(R.color.white);
+            }
+            lastClickId = position;
+        }
+
+    };
+
+
+    public static int getCurrentSelectedItemId(){
+        return lastClickId;
+    }
+
+
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
 
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+            if ( BluetoothDevice.ACTION_FOUND.equals(action) ) {
                 Log.d(LOL, "ресивер");
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                if ( !mDevices.contains(device.getName()) ) {
+                if ( !mDevices.contains(device.getName()) && device.getName() != null) {
                     mDevices.add(device.getName());
                     arrayAdapter.add(device.getName());
                     Log.d(LOL, device.getName());
                 }
                 listView.setAdapter(arrayAdapter);
             }
-
         }
     };
 }
