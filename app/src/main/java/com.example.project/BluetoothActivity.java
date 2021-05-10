a204653228f5357743234b2fd09456beefc9ef40
vovusprog <vovashevalev@gmail.com>
2021-05-10 16:58:59 +0300

    Переделано все

--- app/src/main/java/com.example.project/BluetoothActivity.java
+++ app/src/main/java/com.example.project/BluetoothActivity.java
@@ -1,5 +1,6 @@
 package com.example.project;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.*;
@@ -8,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.BlendMode;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,7 +19,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
 
 import com.example.arduino_controller_v2.R;
 
@@ -26,96 +31,151 @@ import java.util.List;
 import java.util.Set;
 
 public class BluetoothActivity extends Activity {
-    private static final String LOL = "MAIN_LOL";
+    private static final String MAIN_LOL = "MAIN_LOL";
 
-    Button bt;
-    ListView listView;
-    BluetoothAdapter bluetoothAdapter;
-
-    private ArrayList<String> mDevices = new ArrayList<>();
-
+    private Button bt_search;
+    private ListView listView;
     private ArrayAdapter<String> arrayAdapter;
+    private ArrayAdapter<String> arrayAdapter2;
+    private ArrayList devices;
+    private Set<BluetoothDevice> bDevices;
+    private View header1;
+    private View header2;
+    private View footer1;
+    private View footer2;
+    //for bt
+    private BluetoothAdapter mBluetoothAdapter;
+    private BroadcastReceiver mReceiver;
 
-    private Set<BluetoothDevice> devices;
-
-    private static int lastClickId = -1;
-
-
-    public void onCreate(Bundle savedInstanceState) {
+    //_________________________________________________________________________________________________________________________________________________________
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bluetooth);
+        setContentView(R.layout.activity_main);
 
-        Log.d(LOL, "CJPLFYJ");
-        bt = findViewById(R.id.button);
-        listView = findViewById(R.id.listView);
-
-        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1);
-
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if( !bluetoothAdapter.isEnabled() ){
+        bt_search = (Button) findViewById(R.id.button);
+        listView = (ListView) findViewById(R.id.listView);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        arrayAdapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        devices = new ArrayList();
+        header1 = createHeader("Подключенные устройства");
+        header2 = createHeader("Доступные устройства");
+        footer1 = createFooter("");
+        footer2 = createFooter("");
+        //=================================================================================================================================================
+        if( !mBluetoothAdapter.isEnabled() ){
             Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(intent, RESULT_OK);
         }
-        devices = bluetoothAdapter.getBondedDevices();
-        for ( BluetoothDevice bd: devices ){
-            arrayAdapter.add(bd.getName());
+        bDevices = mBluetoothAdapter.getBondedDevices();
+        for (BluetoothDevice bD: bDevices){
+            arrayAdapter.add(bD.getName());
+            arrayAdapter2.add(bD.getName());
+            Log.d(MAIN_LOL, "conjugate " + bD.getName());
         }
-        listView.setAdapter(arrayAdapter);
-
-
-
-        if ( bluetoothAdapter.isDiscovering() ) {
-            bluetoothAdapter.cancelDiscovery();
-            bluetoothAdapter.startDiscovery();
-        }
-
-
-        IntentFilter filter = new IntentFilter();
-
-        filter.addAction(BluetoothDevice.ACTION_FOUND);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-
-        registerReceiver(mReceiver, filter);
-
-        if ( !bluetoothAdapter.isDiscovering() ){
-            bluetoothAdapter.startDiscovery();
-        }
-    }
-
-     public void onDestroy () {
-            unregisterReceiver(mReceiver);
-            super.onDestroy();
-    }
-
-
-    private AdapterView.OnItemClickListener listener = new AdapterView.OnItemClickListener() {
-
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-        }
-
-
-    };
-
-
-
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            final String action = intent.getAction();
-
-            if ( BluetoothDevice.ACTION_FOUND.equals(action) ) {
-                Log.d(LOL, "ресивер");
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                if ( !mDevices.contains(device.getName()) && device.getName() != null) {
-                    mDevices.add(device.getName());
-                    arrayAdapter.add(device.getName());
-                    Log.d(LOL, device.getName());
+        fillList1();
+        //=================================================================================================================================================
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String device_name = (String) listView.getItemAtPosition(position);
+                Log.d(MAIN_LOL, "Choice device: " + device_name);
+            }
+        });
+        //=================================================================================================================================================
+        View.OnClickListener listener = new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                checkPermissionLocation();
+                if (mBluetoothAdapter.isDiscovering()) {
+                    mBluetoothAdapter.cancelDiscovery();
                 }
-                listView.setAdapter(arrayAdapter);
+                mBluetoothAdapter.startDiscovery();
+            }
+        };
+        bt_search.setOnClickListener(listener);
+        //=================================================================================================================================================
+        mReceiver = new BroadcastReceiver(){
+            public void onReceive(Context context, Intent intent)
+            {
+                String action = intent.getAction();
+                Log.d(MAIN_LOL, "searches");
+                if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    Log.d(MAIN_LOL, "searching device");
+                    Toast toast = Toast.makeText(getApplicationContext(), "Поиск устройств", Toast.LENGTH_SHORT);
+                    toast.show();
+                    BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    Log.d(MAIN_LOL, String.valueOf(device.getName()));
+                    if ( !devices.contains(device.getName()) && device.getName() != null) {
+                        devices.add(device.getName());
+                        arrayAdapter.add(device.getName());
+                        listView.addHeaderView(header2, arrayAdapter, false);
+                        listView.addFooterView(footer2, arrayAdapter, false);
+                        listView.setAdapter(arrayAdapter);
+                    }
+                }
+                if(BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action))
+                {
+                    Log.d(MAIN_LOL, "start searching device");
+                    Toast toast = Toast.makeText(getApplicationContext(), "Начал поиск устройств", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                    Log.d(MAIN_LOL, "finished searching device");
+                    Toast toast = Toast.makeText(getApplicationContext(), "Закончил поиск устройств", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        };
+        //=================================================================================================================================================
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        registerReceiver(mReceiver, filter);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(mReceiver, filter);
+        //=================================================================================================================================================
+    }
+    //_________________________________________________________________________________________________________________________________________________________
+    public void onDestroy()
+    {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+    //_________________________________________________________________________________________________________________________________________________________
+    View createHeader(String text) {
+        View v = getLayoutInflater().inflate(R.layout.header, null);
+        ((TextView)v.findViewById(R.id.Text)).setText(text);
+        return v;
+    }
+    //_________________________________________________________________________________________________________________________________________________________
+    View createFooter(String text) {
+        View v = getLayoutInflater().inflate(R.layout.footer, null);
+        ((TextView)v.findViewById(R.id.Text)).setText(text);
+        return v;
+    }
+    //_________________________________________________________________________________________________________________________________________________________
+    void fillList1() {
+        listView.addHeaderView(header1, arrayAdapter2, false);
+        listView.addFooterView(footer1, arrayAdapter2, false);
+        listView.setAdapter(arrayAdapter2);
+    }
+    //_________________________________________________________________________________________________________________________________________________________
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkPermissionLocation() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            int check = checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+            check += checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+
+            if (check != 0) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1002);
             }
         }
-    };
+    }
+//_________________________________________________________________________________________________________________________________________________________
 }
+
